3aee1a293a5ba3bfc8c34e282e85835c3e96102c
Cli-Michael <michaelclinton511@gmail.com>
2022-12-27 16:20:05 +0530

    Init Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+    return  (
+        //  React Fragment <> </> 
+        //  React Fragment <React.Fragment> </React.Fragment> 
+      <> 
+        <h1>Welcome to React Vite Micro App!</h1>
+        <p>Hard to get more minimal than</p>
+      </>
+    );
+}
+
+export default App;
