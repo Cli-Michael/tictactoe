de55bcaa1f9a5dd374cd7904a45dc56c2dc7fd1b
Cli-Michael <michaelclinton511@gmail.com>
2022-12-27 19:14:53 +0530

    Added SAAS, GET to know styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Board from './components/Board'
+import Board from "./components/Board"
+import "./styles/root.scss"
 
 const App = () => {
     return  (
         //  React Fragment <> </> 
         //  React Fragment <React.Fragment> </React.Fragment> 
-      <div> 
-        <h1>TIC TAC TOE</h1>
+      <div className="app"> 
+        <h4>TIC TAC TOE</h4>
         <Board />
       </div>
     );
